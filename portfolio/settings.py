93b3a3c331e3b962914b5cc4c3d379c4abcf7512
MuhammadRazi99 <muhammedraziurrehman@gmail.com>
2025-12-01 00:29:56 +0500

    update media url in settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -19,10 +19,15 @@ load_dotenv(dotenv_path=dotenv_path)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# for debug environment
+# STATIC_URL = '/uploads/'  # URL for media files
+# STATIC_ROOT = os.path.join(BASE_DIR, 'uploads') 
+
+# for production environment
 # MEDIA_URL = '/uploads/'  # URL for media files
-STATIC_URL = '/uploads/'  # URL for media files
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads') 
-STATIC_ROOT = os.path.join(BASE_DIR, 'uploads') 
+MEDIA_URL = '/'  # URL for media files
+MEDIA_ROOT = BASE_DIR 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
